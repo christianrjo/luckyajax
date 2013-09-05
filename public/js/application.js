66730d926674bba6e7045a55c60d202f97d3202b
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-04 17:50:47 -0700

    finished lucky ajax

--- public/js/application.js
+++ public/js/application.js
@@ -7,4 +7,19 @@ $(document).ready(function () {
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 
+$('form').on('submit',function(e) {
+  e.preventDefault();
+  // console.log('button');
+  $.ajax({
+    type: this.method,              
+    url: this.action,
+    data: $(this).serialize()              
+  }).done(function(server_data) {
+    console.log("SUCCESS -- server returned: " + server_data);
+    $("#die").html(server_data);  // use the jQuuery .html() method to add the partial
+  }).fail(function(){
+    console.log('fail');
+  });
+});
+
 });
